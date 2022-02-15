ad1146bd92d629fcdb0a89742e6a57b8a8cc39bc
hitesh-coder <kotechahitesh517@gmail.com>
2022-02-15 16:09:55 +0530

    initial commit

---
+++ SepratePages/pages/Sidebar.js
@@ -0,0 +1,4 @@
+import { createApp } from "vue";
+import Sidebar from "../components/Sidebar.vue";
+
+createApp(Sidebar).mount("#app");
---
+++ web/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1184px-Vue.js_Logo_2.svg.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+img {
+  width: 100px;
+  margin-top: 20px;
+}
+</style>
